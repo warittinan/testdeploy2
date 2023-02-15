d4fb55df95171a2f0d5881dd3a42ae43dbc491c2
warittinan <warittinan.khun@kmutt.ac.th>
2023-02-15 20:55:48 +0700

    set leaw mung

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <div v-if="!gameStr" class="h-screen bg-gray-400 flex flex-col bg-[url('/bg/gg.gif')]"
       style="background-size: 100%;">
       <!--Screen-->
-      <div v-if="startBtn && !gameStr" class="flex justify-end">
+      <div v-if="startBtn && countdownzone " class="flex justify-end">
         <!--Back btn-->
         <div class="bg-white m-4 w-28 h-16 rounded-2xl">
           <button
@@ -18,10 +18,11 @@
       <div class="flex flex-col items-center " v-if="!bg">
         <!--Play Screen-->
         <!-- Max sp -->
-        <img class=" max-lg:hidden" :class="startBtn == true ? '-mt-48' : ''" src="/bg/logo-3.png" style="width: 70em; height: fit-content;">
+        <img v-if="!startBtn && countdownzone " class=" max-lg:hidden" :class="startBtn && countdownzone ? '-mt-48' : ''" src="/bg/logo-3.png" style="width: 70em; height: fit-content;">
+        <img v-if="startBtn && countdownzone " class=" max-lg:hidden" :class="startBtn && countdownzone ? '-mt-48' : ''" src="/bg/logo-3.png" style="width: 70em; height: fit-content;">
         <!-- Min sp -->
-        <img v-if="!startBtn" class=" mt-48 lg:hidden" src="/bg/logo-3.png" style="width: 70em; height: fit-content;">
-        <img v-if="startBtn" class="  lg:hidden" src="/bg/logo-3.png" style="width: 70em; height: fit-content;">
+        <img v-if="!startBtn && countdownzone" class=" mt-48 lg:hidden" src="/bg/logo-3.png" style="width: 70em; height: fit-content;">
+        <img v-if="startBtn && countdownzone" class="  lg:hidden" src="/bg/logo-3.png" style="width: 70em; height: fit-content;">
         <div class="flex flex-col -mt-48">
           <!--Menu-->
           <!-- Max sp -->
@@ -32,7 +33,7 @@
               <span class="text-white text-font text-3xl px-5">PLAY</span>
             </button>
             <button class="btn h-16 hover:border-white hover:bg-emerald-500 border-white rounded-2xl border-8"
-              @click="Cr">
+              @click="Crosshairsetting">
               <span class="text-white text-font text-3xl px-5">CROSSHAIR</span>
             </button>
           </div>
@@ -47,39 +48,39 @@
             </div>
             
             <button class="btn h-16   hover:border-white hover:bg-emerald-500 border-white rounded-2xl border-8"
-              @click="Cr">
+              @click="Crosshairsetting">
               <span class="text-white text-font text-3xl px-5">CROSSHAIR</span>
             </button>
           </div>
           <!-- Level btn -->
           <!-- Max sp -->
-          <div class="flex flex-col space-y-10 max-lg:hidden -mt-10 " v-if="startBtn && !gameStr && !cross">
+          <div class="flex flex-col space-y-10 max-lg:hidden -mt-10 " v-if="startBtn && !gameStr && !cross && countdownzone">
             <div class="rounded-2xl p-4 border-8 bg-black">
               <h1 class="text-slate-50 text-font text-3xl font-bold text-center">
                 LEVEL
               </h1>
             </div>
-            <div v-if="startBtn && !gameStr" class="rounded-2xl bg-white">
+            <div v-if="startBtn && countdownzone" class="rounded-2xl bg-white">
               <button class="btn hover:bg-green-500 border-transparent hover:border-transparent m-2 bg-green-600 w-32"
-                @click="[gameStart(), diffical('easy')]">
+                @click="[countdown(), diffical('easy')]">
                 <span class="text-slate-50 text-font text-xl ">easy</span>
               </button>
             </div>
-            <div v-if="startBtn && !gameStr" class="rounded-2xl bg-white">
+            <div v-if="startBtn && countdownzone" class="rounded-2xl bg-white">
               <button class="btn hover:bg-yellow-400 border-transparent hover:border-transparent m-2 bg-yellow-500 w-32"
-                @click="[gameStart(), diffical('normal')]">
+                @click="[countdown(), diffical('normal')]">
                 <span class="text-slate-50 text-font text-xl">normal</span>
               </button>
             </div>
-            <div v-if="startBtn && !gameStr" class="rounded-2xl bg-white">
+            <div v-if="startBtn && countdownzone" class="rounded-2xl bg-white">
               <button class="btn hover:bg-red-500 border-transparent hover:border-transparent m-2 bg-red-800 w-32"
-                @click="[gameStart(), diffical('hard')]">
+                @click="[countdown(), diffical('hard')]">
                 <span class="text-slate-50 text-font text-xl">hard</span>
               </button>
             </div>
           </div>
           <!-- Min sp -->
-          <div class="flex flex-col space-y-10 lg:hidden" v-if="startBtn && !gameStr && !cross">
+          <div class="flex flex-col space-y-10 lg:hidden" v-if="startBtn && !gameStr && !cross && countdownzone">
             <div class="rounded-2xl p-4 border-8 bg-black">
               <h1 class="text-slate-50 text-font text-3xl font-bold text-center">
                 LEVEL
@@ -87,19 +88,19 @@
             </div>
             <div v-if="startBtn && !gameStr" class="rounded-2xl bg-white">
               <button class="btn hover:bg-green-500 border-transparent hover:border-transparent m-2 bg-green-600 w-32"
-                @click="[gameStart(), diffical('easy')]">
+                @click="[countdown(), diffical('easy')]">
                 <span class="text-slate-50 text-font text-xl ">easy</span>
               </button>
             </div>
             <div v-if="startBtn && !gameStr" class="rounded-2xl bg-white">
               <button class="btn hover:bg-yellow-400 border-transparent hover:border-transparent m-2 bg-yellow-500 w-32"
-                @click="[gameStart(), diffical('normal')]">
+                @click="[countdown(), diffical('normal')]">
                 <span class="text-slate-50 text-font text-xl">normal</span>
               </button>
             </div>
             <div v-if="startBtn && !gameStr" class="rounded-2xl bg-white">
               <button class="btn hover:bg-red-500 border-transparent hover:border-transparent m-2 bg-red-800 w-32"
-                @click="[gameStart(), diffical('hard')]">
+                @click="[countdown(), diffical('hard')]">
                 <span class="text-slate-50 text-font text-xl">hard</span>
               </button>
             </div>
@@ -107,10 +108,17 @@
           <!-- end Level btn -->
         </div>
       </div>
+      <div v-show="!countdownzone" class="h-screen w-screen bg-[url('/bg/zz1.jpg')]" style="background-size: 100% 100%;">
+        <div class=" flex justify-center h-full w-full content-center ">
+          <div class="text-font flex items-center text-9xl text-white">
+            {{ countdowntime }}
+          </div>
+        </div>
+      </div>
       <!-- ===================================================================================================================== -->
-      <div v-if="cross" class="h-full w-full">
+      <div v-if="cross" class="h- w-full ">
         <!--Crosshair Screen-->
-         <!-- Max sp -->
+         <!-- Max sp --> 
          <div class="flex flex-row h-full w-full ">
           <div class="flex w-1/2">
             <div class="m-auto bg-white w-[400px] h-[400px]">
@@ -159,7 +167,7 @@
                 </div>
               </div>
             </div>
-            <!-- cheage color -->
+             
             <div class="grid grid-flow-col justify-center">
               <div class="grid-flow-col rounded-lg mt-32 ">
                 <input type="radio" id="black" value="Black" v-model="CrossColor" class="radio radio-success" />
@@ -179,20 +187,24 @@
                 @click="CrossReset">Reset</button>
             </div>
           </div>
-        </div>
+        </div> 
+<!-- ============================== -->
 
       </div>
     </div>
     <!-- game -->
     <div v-if="gameStr">
-      <div :class="CROSSHAIR" class="bg-gray-400">
+      <div :class="CROSSHAIR" >
         <div v-if="!gameOver" class="h-screen bg-[url('/bg/zz1.jpg')]" style="background-size: 100% 100%;"
           @click="totalClick">
           <div class="flex flex-col" >
-            <div v-if="gameStr && !gameOver" class="flex justify-end">
+            <div v-if="gameStr && !gameOver" class="flex justify-center">
               <!--Timer-->
-              <div class="bg-black m-4 p-3 border-8 rounded-2xl  !important" >
+              <div class=" bg-black m-4 p-3 border-8 rounded-2xl  " >
                 <h1 class="text-white text-font ">Timer : {{ x }}</h1>
+              </div>
+              <div class="bg-black m-4 p-3 border-8 rounded-2xl " >
+                <h1 class="text-white text-font ">Hit : {{ scoreHit }}</h1>
               </div>
             </div>
           </div>
@@ -208,9 +220,9 @@
       <div v-if="gameOver" class="flex flex-col max-d:hidden">
         <div class="h-screen bg-[url('/bg/qq.gif')] ">
           <div class="flex justify-center ">
-            <h class="text-font rounded-3xl mt-4 p-8 border-8 bg-black text-white text-3xl px-10">Results</h>
+            <h class="text-font rounded-3xl mt-24 p-8 border-8 bg-black text-white text-3xl px-10">Results</h>
           </div>
-          <div class="h-full flex flex-row justify-around items-end -mt-36">
+          <div class="h-full flex flex-row justify-around items-end -mt-72">
             <div class="flex flex-col items-start space-y-8">
               <div
                 class="btn rounded-2xl text-font p-4 hover:border-white hover:bg-red-500 text-white bg-black border-white border-8 h-20 w-64 text-center text-2xl pointer-events-auto"
@@ -226,7 +238,7 @@
                   </h1>
                 </div>
                 <div class="absolute m-32  ">
-                  <h1 class="text-font  text-6xl font-bold text-black">10:00</h1>
+                  <h1 class="text-font  text-6xl font-bold text-black">10</h1>
                 </div>
               </div>
               <div class="w-60 h-60 flex justify-center bg-white rounded-2xl">
@@ -263,7 +275,7 @@
               </div>
             </div>
             <div class="flex flex-col items-start space-y-10">
-              <div class="w-72 h-72 mb-32 flex justify-center bg-white rounded-2xl">
+              <div class="w-80 h-72 mb-32 flex justify-center bg-white rounded-2xl">
                 <div class="w-48 h-20 -mt-7 bg-black rounded-2xl">
                   <h1 class="text-font text-3xl text-center items-center p-4 text-white">
                     SCORE
@@ -278,7 +290,7 @@
         </div>
       </div>
       <!-- game over laptop -->
-      <div v-if="gameOver" class="flex flex-col d:hidden">
+       <div v-if="gameOver" class="flex flex-col d:hidden">
         <div class="h-screen bg-[url('/bg/qq.gif')] ">
           <div class="flex justify-center rounded-2xl">
             <h class="text-font rounded-3xl mt-[100px] p-8 border-8 bg-black text-white text-3xl px-10">Results</h>
@@ -286,7 +298,7 @@
           <div class="h-full flex flex-row justify-around items-end -mt-80">
             <div class="flex flex-col items-start space-y-8">
               <div
-                class="text-font btn rounded-2xl p-4 hover:border-white border-white border-8 h-20 w-50 text-center text-l pointer-events-auto"
+                class="text-font btn rounded-2xl p-4 hover:border-white hover:bg-red-500 bg-black border-white border-8 h-20 w-50 text-center text-l pointer-events-auto"
                 @click="reset">
                 BACK TO MENU
               </div>
@@ -299,7 +311,7 @@
                   </h1>
                 </div>
                 <div class="absolute m-16">
-                  <h1 class="text-font text-3xl font-bold text-black">10:00</h1>
+                  <h1 class="text-font text-3xl font-bold text-black">10</h1>
                 </div>
               </div>
               <div class="w-40 h-40 flex justify-center bg-white rounded-2xl">
@@ -349,11 +361,10 @@
             </div>
           </div>
         </div>
-      </div>
+      </div> 
     </div>
   </div>
 </template>
-
 <script setup>
 import data from "/src/data/data.json";
 import { ref, computed } from "vue";
@@ -374,16 +385,10 @@ const CrossColor = ref("Black");
 const clickT = ref(0);
 const scrore = ref(0);
 const type = ref('P')
+const countdowntime = ref(3)
+const countdownzone = ref(true)
 const crossData = computed(() => {
-  for (let i = 0; i < data.length; i++) {
-    if (data[i].type == type.value) {
-      for (let j = 0; j < data[i].color?.length; j++) {
-        if (data[i].color[j].id === CrossColor.value) {
-          return data[i].color[j].image;
-        }
-      }
-    }
-  }
+  return data.find((crs)=>crs.type == type.value).color.find((cx)=>cx.id==CrossColor.value).image
 })
 const CROSSHAIR = computed(() => {
   for (let i = 0; i < data.length; i++) {
@@ -396,6 +401,18 @@ const CROSSHAIR = computed(() => {
     }
   }
 })
+function countdown() {
+  countdownzone.value = false;
+  let time = setInterval(() => {
+    --countdowntime.value;
+    if(countdowntime.value == 0) {
+      gameStart()
+      countdownzone.value = true;
+      return clearInterval(time);
+    }
+  }, 1000);
+  
+}
 function CrossReset() {
   type.value = 'P';
   CrossColor.value = "Black";
@@ -417,7 +434,7 @@ function totalClick() {
 function start() {
   startBtn.value = true;
 }
-function Cr() {
+function Crosshairsetting() {
   cross.value = true;
   startBtn.value = true;
   bg.value = true;
@@ -431,6 +448,7 @@ function reset() {
   bg.value = false;
   clickT.value = 0;
   scrore.value = 0;
+  countdowntime.value = 3;
 }
 
 function gameStart() {
@@ -441,32 +459,32 @@ function diffical(diffi) {
   switch (diffi) {
     case "easy":
       diff.value = "w-20 h-20";
-      eJump(1200);
+      delay(1200);
       break;
     case "normal":
       diff.value = "w-20 h-20";
-      eJump(900);
+      delay(900);
       break;
     case "hard":
       diff.value = "w-20 h-20";
-      eJump(700);
+      delay(700);
       break;
     default:
       break;
   }
-  x.value = 10;
+  x.value = 30;
 }
 function hit() {
   scoreHit.value++;
   enemy.value = false;
-  scrore.value = scoreHit.value * 12;
+  scrore.value = Math.ceil((scoreHit.value / clickT.value) * 100*acc.value);
 }
 function randomPosition() {
-  targetTop.value = `${Math.floor(Math.random() * (winHeight.value - 200) )}`;
-  targetleft.value = `${Math.floor(Math.random() * (winWidth.value - 100) )}`;
+  targetTop.value = `${Math.floor(Math.random() * (winHeight.value - 500) +200)}`;
+  targetleft.value = `${Math.floor(Math.random() * (winWidth.value - 500) +200)}`;
   enemy.value = true;
 }
-function eJump(time) {
+function delay(time) {
   let textx = setInterval(() => {
     randomPosition();
     while (x.value == 0) {
@@ -486,12 +504,17 @@ function timer() {
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Sriracha&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Rubik+Vinyl&display=swap');
+
 @font-face {
   font-family: 'ka1';
   src: url(/font/1.ttf);
 }
 
 .text-font {
-  font-family: 'ka1';
+  /* font-family: 'ka1'; */
+  /* font-family: 'Rubik Vinyl', cursive; */
+  font-family: 'Sriracha', cursive;
 }
 </style>
